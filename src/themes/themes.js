84a89164ef52af928184c2ce8dcbdaf013222a14
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-02-06 23:50:02 -0300

    Final

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -11,3 +11,16 @@ export const myTheme = {
     }
 }
 
+export const myThemeLight = {
+    colors: {
+        primary: {
+            main: '#7b0909',
+            light: '#b30c0c',
+        },
+        secondary: {
+            main: 'white',
+            light: '#bfbaba',
+        },
+    }
+}
+
